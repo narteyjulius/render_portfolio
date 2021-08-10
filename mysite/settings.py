e6fa0016ab2f528c5a4870b142c3bd01d9df205d
Julius Nartey <julius.nartey.71@gmail.com>
2021-08-10 09:18:45 +0000

    corrected fo

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,8 +33,8 @@ environ.Env.read_env()
 SECRET_KEY= 'django-insecure-q)2$##7$5s@v+a9=1v$@cxkm8-@wx_#yntjt203@-cn9*etw%j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 # DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*', '.herokuapp.com']
