2c837382b43926f3143873156e7dd82fbe688e46
Julius Nartey <julius.nartey.71@gmail.com>
2021-08-10 08:58:01 +0000

    done with forms

--- mysite/settings.py
+++ mysite/settings.py
@@ -173,6 +173,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
--- portfolio/views.py
+++ portfolio/views.py
@@ -9,6 +9,39 @@ from .forms import ContactForm
 
 
 
+# def home_list(request):
+#     home = Home.objects.all()
+#     touch = Touch.objects.all()
+#     project = Project.objects.all()
+
+#     if request.method == 'GET':
+#         form = ContactForm()
+#     else:
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             subject = form.cleaned_data['subject']
+#             from_email = form.cleaned_data['from_email']
+#             message = form.cleaned_data['message']
+
+#             send_mail( subject,
+#                         message, from_email, 
+#                         ['julius.nartey.71@gmail.com'])
+#             try:
+#                 send_mail( subject, 
+#                             message,
+#                             from_email,
+#                             ['julius.nartey.71@gmail.com'])
+#             except BadHeaderError:
+#                 return HttpResponse('Invalid header found.')
+#         return redirect( 'portfolio:home_list')
+
+#     return render(request, 'portfolio/base.html', {'form': form, 
+#                                                     'home':home, 
+#                                                     'touch':touch,
+#                                                     'project':project})
+                                                    
+
+
 def home_list(request):
     home = Home.objects.all()
     touch = Touch.objects.all()
@@ -26,8 +59,6 @@ def home_list(request):
             send_mail( subject,
                         message, from_email, 
                         ['julius.nartey.71@gmail.com'])
-
-
             try:
                 send_mail( subject, 
                             message,
@@ -42,4 +73,3 @@ def home_list(request):
                                                     'touch':touch,
                                                     'project':project})
                                                     
-
