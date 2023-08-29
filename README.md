2651b78e767c8b6b7ef70a64ce9b8d841db8d1cd
Julius Nartey <julius.nartey.71@gmail.com>
2023-08-28 22:21:30 -0400

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# new_port
-
-Please visit http://127.0.0.1:8000/home for the portfolio home_page
+# render_portfolio
--- mysite/settings.py
+++ mysite/settings.py
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sylar_portfolio',
-        'USER': 'sylar',
-        'PASSWORD': 'zT70UNWF6mLckhgAWsXPsVfFw1wpEgus',
-        'HOST': 'ccc.oregon-postgres.render.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'sylar_portfolio',
+#         'USER': 'sylar',
+#         'PASSWORD': 'zT70UNWF6mLckhgAWsXPsVfFw1wpEgus',
+#         'HOST': 'ccc.oregon-postgres.render.com',
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
